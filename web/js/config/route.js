ece046d921972cb2083fb37ded4a0c7ee916e907
jonas <tortato.jonas@gmail.com>
2016-02-27 22:56:03 -0300

    commit front end

--- web/js/config/route.js
+++ web/js/config/route.js
@@ -1,11 +1,22 @@
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'templates/index.html'
+			templateUrl: 'templates/index.html',
+			controller: 'IndexCtrl'
 		})
 
 		.when('/busca/:texto', {
 			templateUrl: 'templates/busca.html',
 			controller: 'BuscaCtrl'
+		})
+
+		.when('/minha_conta', {
+			templateUrl: 'templates/minha_conta.html',
+			controller: 'ContaCtrl'
+		})
+
+		.when('/carrinho', {
+			templateUrl: 'templates/carrinho.html',
+			controller: 'CarrinhoCtrl'
 		})
 });
--- web/js/controller.js
+++ web/js/controller.js
@@ -20,16 +20,169 @@ app.controller('MainCtrl', function ($scope, $rootScope, $location, $app_factory
 		}
 	];
 
+	$scope.carrinho_produtos = [
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		}
+	];
+
 	//metodo para fazer uma busca
 	$scope.href = function (link) {
-		$location.path(link + '/' + arguments[1]);
+		var params = arguments[1] ? arguments[1] : '';
+
+		$location.path(link + '/' + params);
 	};
 });
 
-app.controller('BuscaCtrl', function ($rootScope, $routeParams) {
+app.controller('IndexCtrl', function ($scope) {
+	$scope.produtos = [
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		}
+	];
+});
+
+app.controller('BuscaCtrl', function ($scope, $rootScope, $routeParams) {
+	$scope.palavra_pesquisada = $routeParams.texto;
+
+	$scope.produtos = [
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			img: ''
+		}
+	];
+
 	$rootScope.app.api.departamento.filter($routeParams, function (response) {
 		if(status == 'success') {
 			$scope.produtos = response.filtro;
 		}
 	});
+});
+
+app.controller('ContaCtr', function ($scope) {
+
+});
+
+app.controller('CarrinhoCtrl', function ($scope) {
+	$scope.produtos = [
+		{
+			nome: 'produto',
+			valor: 200,
+			quantidade: 1
+		},
+		{
+			nome: 'produto',
+			valor: 200,
+			quantidade: 1
+		}
+	];
+
+	$scope.total = function (calc_quantidade) {
+		var total = 0;
+
+		$.each($scope.produtos, function (i, produto) {
+			total += produto.valor * (calc_quantidade ? produto.quantidade : 1);
+		});
+
+		return total;
+	};
+
+	$scope.bandeiras = [
+		{
+			nome: 'VISA'
+		},
+		{
+			nome: 'MASTERCARD'
+		}
+	];
 });
