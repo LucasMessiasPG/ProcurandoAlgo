d718ade446aa0253101fbf7cbcfdd99d8ee011ea
jonas <tortato.jonas@gmail.com>
2016-03-31 19:56:16 -0300

    commit front end

--- client/app/components/carrinho/Carrinho.ts
+++ client/app/components/carrinho/Carrinho.ts
@@ -1,4 +1,5 @@
 import {Component} from "angular2/core";
+declare var $:any;
 
 @Component({
     templateUrl: "./app/components/carrinho/carrinho.html"
@@ -6,4 +7,35 @@ import {Component} from "angular2/core";
 
 export class CarrinhoComponent {
 
+    public produtos = [
+        {
+            nome: 'produto1',
+            valor: 200,
+            quantidade: 1
+        },
+        {
+            nome: 'produto2',
+            valor: 200,
+            quantidade: 1
+        }
+    ];
+
+    public bandeiras = [
+        {
+            nome: 'VISA'
+        },
+        {
+            nome: 'MASTERCARD'
+        }
+    ];
+
+    public total(calc_quantidade) {
+        var total = 0;
+
+        $.each(this.produtos, function (i, produto) {
+            total += produto.valor * (calc_quantidade ? produto.quantidade : 1);
+        });
+
+        return total;
+    }
 }
