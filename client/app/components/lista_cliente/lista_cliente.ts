3132350efdffa0d55a860f94d719c21a77970d1b
jonas <tortato.jonas@gmail.com>
2016-06-08 21:25:04 -0300

    ...

---
+++ client/app/components/lista_cliente/lista_cliente.ts
@@ -0,0 +1,30 @@
+import {Component} from "angular2/core";
+import {Headers, Http} from "angular2/http";
+
+declare var $:any;
+
+@Component({
+    templateUrl: '../app/components/lista_cliente/lista_cliente.html'
+})
+
+export class ListaClienteComponent {
+
+    private clientes = [];
+
+    constructor(http: Http) {
+        var headers = new Headers();
+
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+        let user = JSON.parse(localStorage.getItem('user'));
+
+        http.post("http://localhost:8000/cliente/filter", $.param({
+            id_usuario: user.id_usuario
+        }),{headers:headers})
+            .map(res => res.json())
+            .subscribe(data => {
+                this.clientes = data.clientes;
+            });
+    }
+
+}
