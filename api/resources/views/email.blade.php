fb2ac156fee8106acdf748fbf9b4f3a242cead1a
Messias <lucasmessias.pg@outlook.com>
2016-06-09 22:42:53 -0300

    ..

---
+++ api/resources/views/email.blade.php
@@ -0,0 +1,7 @@
+<h4>Ola {{ $message['pessoa']->nome }}</h4>
+<br>
+<p><strong>Email de ProcurandoAlgo</strong></p>
+<p>Voce recebeu uma mensagem:</p>
+<div>
+    <p>{{ $message['msg'] }}</p>
+</div>
